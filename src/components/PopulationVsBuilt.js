88d24273b3b74687776eb60ba73b15dceadd46ff
mzgajner <mato@zgajner.com>
2018-11-03 13:15:49 +0100

    Flip chart types for third chart

--- src/components/PopulationVsBuilt.js
+++ src/components/PopulationVsBuilt.js
@@ -7,19 +7,19 @@ export default {
       labels: ['2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017'],
       datasets: [
         {
-          label: 'Prebivalstvo Ljubljane',
-          yAxisID: 'population',
-          backgroundColor: '#00aeef',
-          borderColor: '#00aeef',
-          data: [267760, 276091, 279653, 280140, 280607, 282994, 285857, 287218, 288307, 288919],
+          label: 'Novozgrajena stanovanja',
+          yAxisID: 'apartments',
+          backgroundColor: '#eaf300',
+          borderColor: '#eaf300',
+          data: [1561, 830, 464, 716, 150, 130, 432, 249, 171, 188],
           type: 'line',
           fill: false,
         },
         {
-          label: 'Novozgrajena stanovanja',
-          yAxisID: 'apartments',
-          backgroundColor: '#eaf300',
-          data: [1561, 830, 464, 716, 150, 130, 432, 249, 171, 188],
+          label: 'Prebivalstvo Ljubljane',
+          yAxisID: 'population',
+          backgroundColor: '#00aeef',
+          data: [267760, 276091, 279653, 280140, 280607, 282994, 285857, 287218, 288307, 288919],
         },
       ],
     };
@@ -27,16 +27,16 @@ export default {
     const options = {
       scales: {
         yAxes: [{
-          id: 'population',
-          type: 'linear',
-          position: 'left',
-          ticks: { min: 260000 },
-          gridLines: { display: false },
-        }, {
           id: 'apartments',
           type: 'linear',
           position: 'right',
           ticks: { beginAtZero: true },
+          gridLines: { display: false },
+        }, {
+          id: 'population',
+          type: 'linear',
+          position: 'left',
+          ticks: { min: 260000 },
           gridLines: { display: false },
         }],
       },
